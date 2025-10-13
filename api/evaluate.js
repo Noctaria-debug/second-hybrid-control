85ee985fde1520cccbd4777d14dbeb6d04ec2456
Noctaria-debug <x37fjmcgm@gmail.com>
2025-10-14 02:25:55 +0900

    feat: minimal evaluate API

---
+++ api/evaluate.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const { sessionId = 'n/a', log = '' } = req.body || {}
+  const scores = {
+    natural: log.length > 0 ? 4.0 : 3.0,
+    honesty: 4.6,
+    consistency: 4.5
+  }
+  return res.status(200).json({ sessionId, scores, oneLine: '最小評価OK' })
+}
